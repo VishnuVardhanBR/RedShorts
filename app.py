ce67da0d8d78d5ded7ed033b276bbc4946dfbd8a
VishnuVardhanBR <mmsvishnu23@gmail.com>
2023-08-26 01:19:46 +0530

    changed uploading process

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from get_posts import get_top_video_post
 from scrape import download_reddit_video
-from upload import upload_video
+from upload import upload_helper
 
 subreddit_name = "HumansBeingBros"
 
@@ -16,8 +16,8 @@ subreddit_name = "HumansBeingBros"
 post_details = get_top_video_post(subreddit_name)
 if(post_details):
     video_params = {
-        'file': post_details['title'],
-        'title': '',
+        'file': '',
+        'title': post_details['title'],
         'description': '',
         'category': '22',
         'keywords': '',
@@ -32,6 +32,6 @@ if(post_details):
 
     if(downloaded_video_path != 'error'):
         video_params['file'] = downloaded_video_path[1]
-        upload_video(video_params)
+        upload_helper(video_params)
     else:
         print("Download Error")
---
+++ upload_script.js
@@ -0,0 +1,27 @@
+const uploader = require('youtube-videos-uploader');
+
+// Retrieve the video_params from the command-line arguments
+const [videoParamsJSON] = process.argv.slice(2);
+const videoParams = JSON.parse(videoParamsJSON);
+
+// Destructure the video_params object
+const { file, title, description, privacyStatus } = videoParams;
+
+// Set up your authentication credentials and other options
+const credentials = { email: process.env.EMAIL, pass: process.env.PASSWORD };
+const video = {
+  path: file,
+  title,
+  description,
+  language: 'english',
+  skipProcessingWait: true,
+  onProgress: (progress) => {
+    console.log('progress', progress);
+  },
+  uploadAsDraft: false,
+  isAgeRestriction: false,
+  isNotForKid: false,
+  publishType: privacyStatus.toUpperCase()
+};
+
+uploader.upload (credentials, [video], {headless:false}).then(console.log)
